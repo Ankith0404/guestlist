b0b467ec6134002d5da58e51264f78b950e6fd52
Ankith0404 <84068632+Ankith0404@users.noreply.github.com>
2021-06-26 11:52:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var Names_guest = document.getElementById("Guest_name").value;
+Names_guest.push("guest_name");
+
+function sort(){
+    guest_name.sort();
+    document.getElementById("Sorted_Name").innerHTML = guest_name;
+
+}
